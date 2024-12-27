a8155d4280155386064a81c17fe004f679c02cd6
kritbovorn <kritbovorn@gmail.com>
2024-12-27 21:41:04 +0700

    initial

---
+++ web/js/footer.js
@@ -0,0 +1,22 @@
+{/* <script src="https://cdn.jsdelivr.net/gh/<your-username>/web/js/FtExamblog.js"></script> */ }
+
+
+function setFooter() {
+    document.querySelector('.copyright').innerHTML = `
+      <div class="text-center">
+        <a href="https://www.facebook.com/groups/exambloggas" target="_blank">
+          <i class="fa-brands fa-facebook fa-2x"></i>
+        </a>
+        <a href="https://twitter.com/your-twitter" target="_blank">
+        <i class="fa-brands fa-twitter fa-2x"></i>
+        </a>
+        <a href="https://instagram.com/your-instagram" target="_blank">
+            <i class="fa-brands fa-instagram fa-2x"></i>
+        </a>
+      </div>
+      <center style="font-size:12px;">
+        Kritbovorn Taweeyossak &copy; 2025 
+        <a style="color:gray;text-decoration:none" href="https://examblog64.krooluang.com/p/examvip.html" target="_blank">Contact</a>
+      </center>
+    `;
+}
